89bc9ea476916a6d9c779841797899935c8ddc65
HossameTor <hossametorchi07@gmail.com>
2023-03-15 14:23:33 +0100

    first commit

---
+++ my-react/src/App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+import Products from './Compsants/Products';
+
+const produits = [
+  {
+    id: 1,
+    title: 'PC Portable Gamer HP VICTUS',
+    price: '7490 DH',
+    thumbnail : 'HP16D0195NF.jpg'
+  },
+  {
+    id: 2,
+    title: 'PC Portable Gamer',
+    price: '2190 DH',
+    thumbnail : 'HP14424U3EA.jpg'
+  },
+  {
+    id: 3,
+    title: 'Pc Portable Chromebook Acer',
+    price: '3640 DH',
+    thumbnail: 'NXATHEF002.jpg'
+  },
+  {
+    id: 4,
+    title: 'PC Portable - HUAWEI',
+    price: '1270 DH',
+    thumbnail: 'HUA6901443442959.jpg'
+  },
+];
+
+function App() {
+  return (
+    <Products produits={produits} />
+  );
+}
+export default App;
